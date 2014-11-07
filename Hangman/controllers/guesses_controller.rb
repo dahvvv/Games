d0124125d07783938232361b7faa0ba08f85e6c4
dahvvv <davidjosephkoenig@gmail.com>
2014-11-07 13:32:02 -0500

    fix order being a string

--- Hangman/controllers/guesses_controller.rb
+++ Hangman/controllers/guesses_controller.rb
@@ -3,9 +3,10 @@ class GuessesController < ApplicationController
   post '/' do
     content_type :json
     guess = Guess.new(params[:guess])
-    guess.order = (params[:order].map { |num| num.to_i }).to_s
+    order_array = params[:order].map { |num| num.to_i }
+    guess.order = order_array.to_s
     guess.save!
-    {letter: guess.letter, in_word: guess.in_word, order: guess.order, id: guess.id}.to_json
+    {letter: guess.letter, in_word: guess.in_word, order: order_array, id: guess.id}.to_json
   end
 
 end
