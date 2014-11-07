14cc302b3da99cd9062bebfb645250e93057a8bf
dahvvv <davidjosephkoenig@gmail.com>
2014-11-07 14:52:36 -0500

    letters disappear upon clicking

--- Hangman/controllers/guesses_controller.rb
+++ Hangman/controllers/guesses_controller.rb
@@ -10,25 +10,3 @@ class GuessesController < ApplicationController
   end
 
 end
-
-
-
-# get '/api/toonify' do
-#   content_type :json
-
-#   accent_sent_from_client = params[:accentFromClient]
-#   sentence_sent_from_client = params[:sentenceFromClient]
-
-#   case accent_sent_from_client
-#   when 'daffy'
-#     sentence_processed_by_server = sentence_sent_from_client.gsub(/s/i, 'th')
-#   when 'elmer'
-#     sentence_processed_by_server = sentence_sent_from_client.gsub(/r/i, 'w')
-#   else
-#     sentence_processed_by_server = sentence_sent_from_client
-#   end
-
-#     {
-#       accent_from_server: accent_sent_from_client,
-#       sentence_from_server: sentence_processed_by_server
-#     }.to_json
