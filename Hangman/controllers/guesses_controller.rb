f5e54175087cd7b012113b2b348b220d379ae9bf
dahvvv <davidjosephkoenig@gmail.com>
2014-11-09 16:11:04 -0500

    hm database logs correct/incorrect guesses

--- Hangman/controllers/guesses_controller.rb
+++ Hangman/controllers/guesses_controller.rb
@@ -3,8 +3,12 @@ class GuessesController < ApplicationController
   post '/' do
     content_type :json
     guess = Guess.new(params[:guess])
-    order_array = params[:order].map { |num| num.to_i }
-    guess.order = order_array.to_s
+    if params[:order] != nil
+      order_array = params[:order].map { |num| num.to_i }
+      guess.order = order_array.to_s
+    else
+      guess.order = [].to_s
+    end
     guess.save!
     {letter: guess.letter, in_word: guess.in_word, order: order_array, id: guess.id}.to_json
   end
